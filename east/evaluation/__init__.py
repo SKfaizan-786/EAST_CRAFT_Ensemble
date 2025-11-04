859fe68852b7ccb61a9c9e91ad8674106741e9e3
SK Faizanuddin <faizanuddinsk56@gmail.com>
2025-11-04 12:33:22 +0530

    Re-structure the folder

--- east/evaluation/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Evaluation and Metrics
-"""
-
-# Will be implemented in Sprint 5
-# from .icdar_eval import ICDAREvaluator
-# from .benchmark import PerformanceBenchmark
-
-__all__ = []
--- east/losses/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Loss Functions for EAST Training
-"""
-
-# Will be implemented in Sprint 4  
-# from .east_loss import EASTLoss
-# from .geometry_loss import GeometryLoss
-
-__all__ = []
